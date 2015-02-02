bca08eab1051233bc37374b9c7c925c6c04837f2
Sam Lau <samlau95@gmail.com>
2015-02-01 18:46:04 -0800

    Finish basic setup script

--- setup.sh
+++ setup.sh
@@ -11,18 +11,22 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 cd $HOME
 
+sudo apt-get update
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
+rvm gemset use global
+gem install tmuxinator
+gem install pry
+rvm gemset use default
 
-sudo apt-get update
 sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
-
 sudo apt-get install libfreetype6 libfreetype6-dev -y
 sudo apt-get install libfontconfig1 libfontconfig1-dev -y
 
+cd $HOME
 export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
 wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-
 sudo mv $PHANTOM_JS.tar.bz2 /usr/local/share/
 cd /usr/local/share/
 sudo tar xvjf $PHANTOM_JS.tar.bz2
@@ -30,5 +34,4 @@ sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomj
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
 
-
-
+cd $HOME
